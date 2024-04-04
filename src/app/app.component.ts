2bb55d4398507453fdf02cd6dbbc45d07e0fa810
Your Parvathi <parvathigvaikom.@gmail.com>
2024-04-04 16:16:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'template-driven-form';
+  form={
+    Username: '',
+    email:'',
+    Address:'',
+    dob:'',
+    password:'',
+    number:''
+  }
+  onSubmit():void{
+    console.log(this.form)
+  }
 }
